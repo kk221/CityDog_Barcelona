7300b3e8de953157559f657940972bb99ff3fe5b
Kexin <30476794+kk221@users.noreply.github.com>
2025-01-08 15:55:29 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{html,js}",
+    "./js/**/*.js",
+    "./*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
